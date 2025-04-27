4af5dc5c46bc4ece89022effa82d2ab8ddd1a88b
Ahmuq <ahlul963@gmail.com>
2025-04-27 21:21:13 +0700

    first commit

---
+++ src/classes/ogbot.ts
@@ -0,0 +1,212 @@
+import chalk from "chalk";
+import { ethers, FetchRequest } from "ethers";
+import fs from "fs";
+import path from "path";
+import { BTC_ABI, ETH_ABI, ROUTER_ABI, USDT_ABI } from "../config/abi";
+import { logMessage } from "../utils/logger";
+import { getProxyAgent } from "./proxy";
+
+const configPath = path.resolve(__dirname, "../../config.json");
+const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+
+export class ogBot {
+  private privkey: string;
+  private web3: any;
+  private wallet: ethers.Wallet;
+  private swapaddress: string = '0x16a811adc55a99b4456f62c54f12d3561559a268';
+  private usdtaddress: string = '0xa8f030218d7c26869cadd46c5f10129e635cd565';
+  private ethaddress: string = '0x2619090fcfdb99a8ccf51c76c9467f7375040eeb';
+  private btcaddress: string = '0x6dc29491a8396bd52376b4f6da1f3e889c16ca85';
+  private usdtContract: any;
+  private swapContract: any;
+  private btcContract: any;
+  private ethContract: any;
+  private RPC: string;
+  private explorer: string;
+  private proxy: string | null;
+  private currentNum: number;
+  private total: number;
+
+
+  constructor(privkey: string, proxy: string | null = null, currentNum: number, total: number) {
+    this.RPC = config.RPC_URL;
+    this.explorer = config.EXPLORE_URL;
+    this.privkey = privkey;
+    this.web3 = this.initializeWeb3();
+    this.wallet = new ethers.Wallet(this.privkey, this.web3);
+    this.usdtContract = new ethers.Contract(this.usdtaddress, USDT_ABI, this.wallet);
+    this.swapContract = new ethers.Contract(this.swapaddress, ROUTER_ABI, this.wallet);
+    this.btcContract = new ethers.Contract(this.btcaddress, BTC_ABI, this.wallet);
+    this.ethContract = new ethers.Contract(this.ethaddress, ETH_ABI, this.wallet);;
+    this.currentNum = currentNum;
+    this.total = total
+    this.proxy = proxy;
+  }
+
+
+  private initializeWeb3() {
+    if (this.proxy) {
+      FetchRequest.registerGetUrl(
+        FetchRequest.createGetUrlFunc({
+          agent: getProxyAgent(this.proxy, this.currentNum, this.total),
+        })
+      );
+      return new ethers.JsonRpcProvider(this.RPC);
+    }
+    return new ethers.JsonRpcProvider(this.RPC);
+  }
+
+  private async approveToken(tokenContract: any, spenderAddress: any, amount: any) {
+    logMessage(this.currentNum, this.total, "Trying approval...", "process");
+    try {
+      const nonce = await this.web3.getTransactionCount(
+        this.wallet.address,
+        "pending"
+      );
+      logMessage(this.currentNum, this.total, `Using nonce ${nonce} for approval`, "process");
+      const tx = await tokenContract.approve(
+        spenderAddress,
+        amount,
+        {
+          nonce,
+          gasLimit: 100000,
+          gasPrice: (await this.web3.getFeeData()).gasPrice,
+        }
+      );
+      await tx.wait();
+      logMessage(this.currentNum, this.total, `Transaction hash : ${tx.hash}`, "success");
+      logMessage(this.currentNum, this.total, `BlockHash URL : ${this.explorer}${tx.hash}`, "success");
+      console.log(chalk.white("-".repeat(85)));
+      return tx.hash;
+    } catch (error: any) {
+      logMessage(this.currentNum, this.total, `Approval failed: ${error.message}`, "error");
+      return null;
+    }
+  }
+
+  public async autoSwapBtcUsdt() {
+    const count = 3;
+    for (let i = 0; i < count; i++) {
+      const random = Math.random() < 0.5 ? 'USDT->BTC' : 'BTC->USDT';
+      const randomUsdt = parseFloat((Math.random() * (2 - 0.5) + 0.5).toFixed(2));
+      const randomBtc = parseFloat((Math.random() * (0.0005 - 0.0002) + 0.0002).toFixed(6));
+      const usdtBalance = await this.usdtContract.balanceOf(this.wallet.address);
+      const btcBalance = await this.btcContract.balanceOf(this.wallet.address);
+      const usdtDecimals = await this.usdtContract.decimals();
+      const btcDecimals = await this.btcContract.decimals();
+
+      const formattedUsdtBalance = ethers.formatUnits(usdtBalance, usdtDecimals);
+      const formattedBtcBalance = ethers.formatUnits(btcBalance, btcDecimals);
+
+      logMessage(this.currentNum, this.total, `Balance Before Swap:`, "info");
+      logMessage(this.currentNum, this.total, `USDT Balance: ${formattedUsdtBalance} USDT`, "info");
+      logMessage(this.currentNum, this.total, `BTC Balance: ${formattedBtcBalance} BTC`, "info");
+
+      console.log(chalk.white("-".repeat(85)));
+
+      try {
+        const tokenToApprove = random === 'USDT->BTC' ? this.usdtContract : this.btcContract;
+        const amountToApprove = random === 'USDT->BTC'
+          ? ethers.parseUnits(randomUsdt.toString(), 6)
+          : ethers.parseUnits(randomBtc.toString(), 8);
+        const approveTx = await this.approveToken(tokenToApprove, this.swapaddress, amountToApprove);
+        if (!approveTx) return;
+        logMessage(this.currentNum, this.total, `Approved ${random === 'USDT->BTC' ? 'USDT' : 'BTC'} token`, "success");
+        const deadline = Math.floor(Date.now() / 1000) + 300;
+        const swapNonce = await this.web3.getTransactionCount(this.wallet.address, "pending");
+        logMessage(this.currentNum, this.total, `Using nonce ${swapNonce} for swap ${random}`, "process");
+        const amountIn = random === 'USDT->BTC'
+          ? ethers.parseUnits(randomUsdt.toString(), 6)
+          : ethers.parseUnits(randomBtc.toString(), 8);
+        const swapTx = await this.swapContract.exactInputSingle({
+          tokenIn: random === 'USDT->BTC' ? this.usdtaddress : this.btcaddress,
+          tokenOut: random === 'BTC->USDT' ? this.usdtaddress : this.btcaddress,
+          fee: 3000,
+          recipient: this.wallet.address,
+          deadline,
+          amountIn,
+          amountOutMinimum: 0,
+          sqrtPriceLimitX96: 0,
+        }, {
+          nonce: swapNonce,
+          gasLimit: 1000000,
+          gasPrice: (await this.web3.getFeeData()).gasPrice,
+        });
+
+        await swapTx.wait();
+        logMessage(this.currentNum, this.total, `Swap ${random} successful`, "success");
+        logMessage(this.currentNum, this.total, `Amount: ${ethers.formatUnits(amountIn, random === 'USDT->BTC' ? 6 : 8)}`, "success");
+        logMessage(this.currentNum, this.total, `Transaction hash: ${swapTx.hash} `, "success");
+        logMessage(this.currentNum, this.total, `BlockHash URL: ${this.explorer}${swapTx.hash} `, "success");
+        console.log(chalk.white("-".repeat(85)));
+
+      } catch (error: any) {
+        logMessage(this.currentNum, this.total, `Auto swap failed: ${error.message} `, "error");
+      }
+    }
+  }
+
+  public async autoSwapEthUsdt() {
+    const count = 3;
+    for (let i = 0; i < count; i++) {
+      const random = Math.random() < 0.5 ? 'USDT->ETH' : 'ETH->USDT';
+      const randomUsdt = parseFloat((Math.random() * (2 - 0.5) + 0.5).toFixed(2));
+      const randomEth = parseFloat((Math.random() * (0.0005 - 0.0002) + 0.0002).toFixed(6));
+      const usdtBalance = await this.usdtContract.balanceOf(this.wallet.address);
+      const ethBalance = await this.ethContract.balanceOf(this.wallet.address);
+      const usdtDecimals = await this.usdtContract.decimals();
+      const ethDecimals = await this.ethContract.decimals();
+
+      const formattedUsdtBalance = ethers.formatUnits(usdtBalance, usdtDecimals);
+      const formattedEthBalance = ethers.formatUnits(ethBalance, ethDecimals);
+
+      logMessage(this.currentNum, this.total, `Balance Before Swap:`, "info");
+      logMessage(this.currentNum, this.total, `USDT Balance: ${formattedUsdtBalance} USDT`, "info");
+      logMessage(this.currentNum, this.total, `ETH Balance: ${formattedEthBalance} ETH`, "info");
+
+      console.log(chalk.white("-".repeat(85)));
+
+      try {
+        const tokenToApprove = random === 'USDT->ETH' ? this.usdtContract : this.ethContract;
+        const amountToApprove = random === 'USDT->ETH'
+          ? ethers.parseUnits(randomUsdt.toString(), 6)
+          : ethers.parseUnits(randomEth.toString(), 8);
+        const approveTx = await this.approveToken(tokenToApprove, this.swapaddress, amountToApprove);
+        if (!approveTx) return;
+        logMessage(this.currentNum, this.total, `Approved ${random === 'USDT->ETH' ? 'USDT' : 'ETH'} token`, "success");
+        const deadline = Math.floor(Date.now() / 1000) + 300;
+        const swapNonce = await this.web3.getTransactionCount(this.wallet.address, "pending");
+        logMessage(this.currentNum, this.total, `Using nonce ${swapNonce} for swap ${random}`, "process");
+        const amountIn = random === 'USDT->ETH'
+          ? ethers.parseUnits(randomUsdt.toString(), 6)
+          : ethers.parseUnits(randomEth.toString(), 8);
+        const swapTx = await this.swapContract.exactInputSingle({
+          tokenIn: random === 'USDT->ETH' ? this.usdtaddress : this.ethaddress,
+          tokenOut: random === 'ETH->USDT' ? this.usdtaddress : this.ethaddress,
+          fee: 3000,
+          recipient: this.wallet.address,
+          deadline,
+          amountIn,
+          amountOutMinimum: 0,
+          sqrtPriceLimitX96: 0,
+        }, {
+          nonce: swapNonce,
+          gasLimit: 1000000,
+          gasPrice: (await this.web3.getFeeData()).gasPrice,
+        });
+
+        await swapTx.wait();
+        logMessage(this.currentNum, this.total, `Swap ${random} successful`, "success");
+        logMessage(this.currentNum, this.total, `Amount: ${ethers.formatUnits(amountIn, random === 'USDT->ETH' ? 6 : 8)}`, "success");
+        logMessage(this.currentNum, this.total, `Transaction hash: ${swapTx.hash} `, "success");
+        logMessage(this.currentNum, this.total, `BlockHash URL: ${this.explorer}${swapTx.hash} `, "success");
+        console.log(chalk.white("-".repeat(85)));
+
+      } catch (error: any) {
+        logMessage(this.currentNum, this.total, `Auto swap failed: ${error.message} `, "error");
+      }
+    }
+  }
+
+
+}
---
+++ src/index.ts
@@ -0,0 +1,84 @@
+import chalk from "chalk";
+import fs from "fs";
+import { ogBot } from "./classes/ogbot";
+import { getRandomProxy, loadProxies } from "./classes/proxy";
+import { logMessage, prompt } from "./utils/logger";
+let totalTransactionGlobal: number | null = null;
+
+async function main(): Promise<void> {
+  console.log(
+    chalk.cyan(`
+░▄▀▄░█▀▀░░░░░█▀▄░█▀█░▀█▀
+░█/█░█░█░▄▄▄░█▀▄░█░█░░█░
+░░▀░░▀▀▀░░░░░▀▀░░▀▀▀░░▀░
+  By : El Puqus Airdrop
+   github.com/ahlulmukh
+ Use it at your own risk
+  `)
+  );
+
+  if (totalTransactionGlobal === null) {
+    const totalTransactionInput = parseInt(await prompt(chalk.yellow("Total transaction perday? ")));
+    totalTransactionGlobal = totalTransactionInput;
+
+    if (isNaN(totalTransactionGlobal) || totalTransactionGlobal <= 0) {
+      console.log(chalk.red("Input not valid. Please input a number greater than 0."));
+      process.exit(1);
+    }
+  }
+
+  const accounts = fs
+    .readFileSync("privatekey.txt", "utf8")
+    .split("\n")
+    .filter(Boolean);
+  const count = accounts.length;
+
+  const proxiesLoaded = loadProxies();
+  if (!proxiesLoaded) {
+    logMessage(null, null, "No Proxy. Using default IP", "warning");
+  }
+
+  let successful = 0;
+
+  for (let i = 0; i < count; i++) {
+    console.log(chalk.white("-".repeat(85)));
+    logMessage(i + 1, count, "Processing Transaction", "process");
+    const privkey = accounts[i];
+    const currentProxy = await getRandomProxy(i + 1, count);
+    const og = new ogBot(privkey, currentProxy, i + 1, count);
+
+    try {
+      let txCount = 0;
+      while (txCount < totalTransactionGlobal) {
+        logMessage(i + 1, count, `Total Transaction: ${txCount + 1}/${totalTransactionGlobal}`, "info");
+        await og.autoSwapBtcUsdt();
+        await og.autoSwapEthUsdt();
+        txCount++;
+      }
+
+      successful++;
+    } catch (err) {
+      logMessage(i + 1, count, `Error: ${(err as any).message}`, "error");
+    }
+  }
+
+  console.log(chalk.white("-".repeat(85)));
+  const now = new Date();
+  const targetTime = new Date(now);
+  targetTime.setHours(12, 0, 0, 0);
+
+  if (now > targetTime) {
+    targetTime.setDate(targetTime.getDate() + 1);
+  }
+
+  const sleepTime = targetTime.getTime() - now.getTime();
+  logMessage(null, null, `Sleeping until ${targetTime.toISOString()}...`, "success");
+  await new Promise(resolve => setTimeout(resolve, sleepTime));
+
+  main();
+}
+
+main().catch((err) => {
+  console.error(chalk.red("Error occurred:"), err);
+  process.exit(1);
+});
